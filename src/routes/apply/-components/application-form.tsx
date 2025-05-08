9c0c731421f4aa49ec71006d739fd2a308246f8c
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2025-05-08 21:36:58 +0800

    take 2

--- src/routes/apply/-components/application-form.tsx
+++ src/routes/apply/-components/application-form.tsx
@@ -1,3 +1,26 @@
-
-
-
+const ApplicationForm = () => {
+  return (
+    <div>
+      <h1>Application Form</h1>
+      <form>
+        <label>
+          Name:
+          <input type='text' name='name' />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type='email' name='email' />
+        </label>
+        <br />
+        <label>
+          Resume:
+          <input type='file' name='resume' />
+        </label>
+        <br />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
+export default ApplicationForm;
--- src/routes/apply/index.tsx
+++ src/routes/apply/index.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from '@tanstack/react-router';
+import ApplicationForm from './-components/application-form';
 
-export const Route = createFileRoute("/apply/")({
+export const Route = createFileRoute('/apply/')({
   component: RouteComponent,
 });
 
